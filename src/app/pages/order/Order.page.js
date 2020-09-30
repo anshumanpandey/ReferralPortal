88c1f7ad7b31b5428914e95aae3e2e145948ae95
leo <leo_9_t@hotmail.com>
2020-09-30 19:13:39 -0400

    ADDED search input

--- src/app/pages/order/Order.page.js
+++ src/app/pages/order/Order.page.js
@@ -1,15 +1,34 @@
 import React, { useState, useEffect } from "react";
 import useAxios from 'axios-hooks'
 import DataTable from 'react-data-table-component';
+import { TextField } from "@material-ui/core";
 
 export const OrderPage = () => {
+  const [valueToSearch, setValueToSearch] = useState("");
+  const [dataToShow, setDataToShow] = useState([]);
   const [{ data, loading, error }, refetch] = useAxios({
     url: '/order'
   }, { manual: true })
 
   useEffect(() => {
     refetch()
-  },[])
+  }, [])
+
+  useEffect(() => {
+    if (data && !loading) {
+      setDataToShow(data)
+    }
+  }, [loading])
+
+  useEffect(() => {
+    if (data && !loading) {
+      if (valueToSearch){
+        setDataToShow(data.filter(r => r.Customer.firstname.includes(valueToSearch)))
+      } else {
+        setDataToShow(data)
+      }
+    }
+  }, [valueToSearch])
 
   return (
     <>
@@ -17,8 +36,21 @@ export const OrderPage = () => {
         <div className="col-md-12">
           <DataTable
             progressPending={loading}
-            data={data}
+            data={dataToShow}
             pagination={true}
+            noHeader={true}
+            subHeader={true}
+            subHeaderComponent={
+              <div>
+                <TextField
+                  size={"small"}
+                  placeholder="Customer Name"
+                  margin="normal"
+                  value={valueToSearch}
+                  onChange={(e) => setValueToSearch(e.target.value)}
+                />
+              </div>
+            }
             columns={[
               { sortable: true, name: 'Order Amount', selector: 'orderAmount' },
               {
@@ -30,7 +62,7 @@ export const OrderPage = () => {
               {
                 sortable: true,
                 name: 'Referred By',
-                cell: (row) => row.Customer.Customers.length == 0 ? 'N/A': `${row.Customer.Customers[0].firstname} ${row.Customer.Customers[0].lastname}`
+                cell: (row) => row.Customer.Customers.length == 0 ? 'N/A' : `${row.Customer.Customers[0].firstname} ${row.Customer.Customers[0].lastname}`
               },
               {
                 sortable: true,
